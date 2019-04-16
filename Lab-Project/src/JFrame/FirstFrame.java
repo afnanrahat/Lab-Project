db0bf94c40f519b1b780e683dccc3c10f98e69b9
Afnan Rahat <afnan.rahat@northsouth.edu>
2019-04-16 21:21:58 +0600

    Package

--- Lab-Project/src/JFrame/FirstFrame.java
+++ Lab-Project/src/JFrame/FirstFrame.java
@@ -6,6 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FirstFrame extends JFrame {
 
@@ -35,8 +41,36 @@ public class FirstFrame extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("WELCOME TO THE ACTIVE QUIZ SYSTEM");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 21));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 22, 416, 40);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnTeacher = new JButton("TEACHER");
+		btnTeacher.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SecondFrame sf = new SecondFrame();
+				sf.SFrame();
+				setVisible(false);
+			}
+		});
+		btnTeacher.setBounds(75, 108, 107, 60);
+		contentPane.add(btnTeacher);
+		
+		JButton btnStudent = new JButton("STUDENT");
+		btnStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SecondFrame sf = new SecondFrame();
+				sf.SFrame();
+				setVisible(false);
+			}
+		});
+		btnStudent.setBounds(248, 110, 107, 56);
+		contentPane.add(btnStudent);
 	}
 
 }
